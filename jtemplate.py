0158dc02a3d47d6d9ade97b6fc06227c61c47563
Pradish Bijukchhe <pradishbijukchhe@gmail.com>
2018-11-24 23:42:44 +0545

    Fixes all style reseting bug

--- jtemplate.py
+++ jtemplate.py
@@ -8,6 +8,8 @@ import sys
 import pystache
 import string
 import json
+import re
+import copy
 sys.path.append('/usr/share/inkscape/extensions')
 
 # We will use the inkex module with the predefined Effect base class.
@@ -19,7 +21,7 @@ class JTemplateEffect(inkex.Effect):
         inkex.Effect.__init__(self)
 
         self.OptionParser.add_option('-j', '--json', action='store',
-                                     type='string', dest='json', default='default.json',
+                                     type='string', dest='json', default='examples/invoice.json',
                                      help='Json file to template')
 
     def effect(self):
@@ -28,33 +30,71 @@ class JTemplateEffect(inkex.Effect):
         json_data = json.loads(json_file.read())
 
         svg = self.document.getroot()
+
+        # FOR SECTIONS
         textElements = svg.xpath(
             '//svg:text', namespaces=inkex.NSS)
 
         for e in textElements:
-            style = e.get('style')
-            line_height = inkex.unittouu(parseStyle(style)["line-height"])
-            font_size = inkex.unittouu(parseStyle(style)["font-size"])
-            font_spacing = font_size * line_height
-            x = e.get('x')
-            y = inkex.unittouu(e.get('y'))
-
             old_text = string.join(e.xpath(".//text()"), "\n")
-            for tspan in e.getchildren():
-                e.remove(tspan)
+            match = re.match(r'{{#([^}}]*)}}[\S\s]*{{\/\1}}', old_text)
+            if match != None:
+                style = e.get('style')
+                line_height = inkex.unittouu(parseStyle(style)["line-height"])
+                font_size = inkex.unittouu(parseStyle(style)["font-size"])
+                font_spacing = font_size * line_height
+                x = e.get('x')
+                y = inkex.unittouu(e.get('y'))
+                for tspan in e.getchildren():
+                    e.remove(tspan)
 
-            new_text = pystache.render(old_text, json_data)
+                new_text = pystache.render(old_text, json_data)
 
-            lines = new_text.split("\n")
+                lines = new_text.split("\n")
 
-            for line in lines:
-                tspan = inkex.etree.Element(inkex.addNS('tspan', 'svg'), attrib={
-                    inkex.addNS('role', 'sodipodi'): 'line',
-                    'x': x,
-                    'y': str(y)})
-                tspan.text = line + "\n"
-                e.append(tspan)
-                y += font_spacing
+                for line in lines:
+                    tspan = inkex.etree.Element(inkex.addNS('tspan', 'svg'), attrib={
+                        inkex.addNS('role', 'sodipodi'): 'line',
+                        'x': x,
+                        'y': str(y)})
+                    tspan.text = line + "\n"
+                    e.append(tspan)
+                    y += font_spacing
+
+        # FOR NORMAL VARIABLES
+        textElements = svg.xpath(
+            '//svg:text//text()', namespaces=inkex.NSS)
+
+        for e in textElements:
+            old_text = e.getparent().text
+            mustache_section = re.match('{{[#/][^}}]*}}', old_text)
+            if mustache_section == None:
+                new_text = pystache.render(old_text, json_data)
+                e.getparent().text = new_text
+
+        # flowElements = svg.xpath(
+        #     '//svg:flowPara/text()', namespaces=inkex.NSS)
+
+        # for e in flowElements:
+        #     parent = e.getparent()
+        #     old_text = parent.text
+        #     mustache_section = re.match('{{#[^}}]*}}.*{{\/[^}}]*}}', old_text)
+
+        #     if mustache_section != None:
+        #         root_element = parent.getparent()
+        #         # e.getparent().getparent().remove(e.getparent())
+
+        #         old_text = old_text.replace("{{ln}}", "\n")
+        #         new_text = pystache.render(old_text, json_data)
+
+        #         lines = new_text.split("\n")
+
+        #         for line in lines:
+        #             para_temp = copy.deepcopy(parent)
+        #             para_temp.text = line
+        #             root_element.insert(
+        #                 root_element.index(parent), para_temp)
+        #         root_element.remove(parent)
 
 
 effect = JTemplateEffect()
